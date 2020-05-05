bc652e47abc4e84a2caf668071fcd4873e2bf982
Uchey <ucheuzochukwumark@gmail.com>
2020-05-05 08:13:38 +0100

    initiate get user endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,65 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { DatabaseService } from './database/db.service';
+import { LoginDto } from './dto/login.dto';
+import { RegisterDto } from './dto/register.dto';
+import { get } from 'http';
+import { async } from 'rxjs/internal/scheduler/async';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly databaseService: DatabaseService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('register')
+  async register(@Body() body: RegisterDto) {
+    const { email, name, password } = body;
+    const isUserExist = await this.databaseService.find({ email }, 'auths');
+    if (isUserExist) {
+      throw new HttpException(
+        `You are already registered with us.`,
+        HttpStatus.NOT_ACCEPTABLE,
+      );
+    }
+
+    const createdUser = await this.databaseService.add(body, 'auths');
+    return createdUser;
   }
+
+  @Post('login')
+  async login(@Body() body: LoginDto) {
+    const { email, password } = body;
+    const user = await this.databaseService.find({ email, password }, 'auths');
+    if (!user) {
+      throw new HttpException(
+        `You are not registered with us.`,
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
+
+    return user;
+  }
+
+  @Get('profile')
+  async getUsers(@Body() any){
+    const { name } = any;
+    const user = await this.databaseService.find({ name }, 'auths');
+    if (!user) {
+      throw new HttpException(
+        `User profile not found in the database.`,
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
+
+    return user;
+    
+  }
+
+
+
 }
