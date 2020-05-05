495d7cb8353eba71ba1ab50ecc8b14472229fa49
Uchey <ucheuzochukwumark@gmail.com>
2020-05-05 07:31:42 +0100

    finalise db service

--- src/database/db.service.ts
+++ src/database/db.service.ts
@@ -22,5 +22,40 @@ export class DatabaseService {
     }
   }
 
-  
+  async findAll(dataStore: DataStore): Promise<any> {
+    const getUsers = await this.db.get(dataStore).value();
+    return getUsers;
+  }
+
+  async find(condition: object, dataStore: DataStore): Promise<any> {
+    const values = await this.db.get(dataStore).find(condition).value();
+    return values;
+  }
+
+  async update(
+    key: string,
+    value: string | String,
+    dataStore: string,
+    dataUpdate: any,
+  ): Promise<any> {
+    const getUsers = await this.db.get(dataStore).value();
+    let modify;
+    const listData = getUsers.map(user => {
+      if (user[key] !== value) return user;
+      if (user[key] === value) {
+        modify = Object.assign(user, dataUpdate);
+        return modify;
+      }
+    });
+    await this.db.set(dataStore, listData).write();
+    return modify;
+  }
+
+  async add(record: any, dataStore: DataStore): Promise<any> {
+    const getData = await this.db.get(dataStore).value();
+    record.id = uuid.v1();
+    getData.push(record);
+    await this.db.set(dataStore, getData).write();
+    return record;
+  }
 }
